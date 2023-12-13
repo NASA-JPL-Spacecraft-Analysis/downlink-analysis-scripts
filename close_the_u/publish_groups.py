eb9fc5cf62b0e959806e523d8d139594f3e394f4
Alex Alcazar <alcazar@mt-207601.jpl.nasa.gov>
2023-12-12 23:21:23 -0800

    supports channel definitions with appropriate mapped enums

--- close_the_u/publish_groups.py
+++ close_the_u/publish_groups.py
@@ -15,14 +15,17 @@ def parse_channel(data: dict) -> list:
     top_level_key = 'telemetry_dictionary'
     group_key = 'telemetry_groups'
     telemetry_key = 'telemetry_definitions'
-    groups = parse_dict(data, top_level_key, group_key, telemetry_key)
+    enum_key = 'enum_definitions'
+    groups = parse_dict(data, top_level_key, group_key, telemetry_key, enum_key)
     telemetry_list = []
     
     # add telemetry mapping of short name and name
     for key in groups['telemetry']:
         telemetry_definition = {
             'abbreviation': None,
-            'name': None
+            'name': None,
+            'enum_name': None,
+            'enum_symbols': []
         }
         if isinstance(key, dict):
                 for sub_key, value in key.items():
@@ -30,6 +33,23 @@ def parse_channel(data: dict) -> list:
                         telemetry_definition['abbreviation'] = value
                     elif sub_key == '@name':
                         telemetry_definition['name'] = value
+                    elif sub_key == 'enum_format':
+                        telemetry_definition['enum_name'] = value['@enum_name']
+                        enum_name = telemetry_definition['enum_name']
+                        for key in groups['enum_table']:
+                            if isinstance(key, dict):
+                                for sub_key, value in key.items():
+                                    if sub_key == '@name' and value == enum_name:
+                                        # access values from the inner dictionary
+                                        inner_values = key.get('values', {}).get('enum', [])
+                                        enum_symbols = []
+                                        for item in inner_values:
+                                            if isinstance(item, dict):
+                                                for sub_key, value in item.items():
+                                                    if sub_key == '@symbol':
+                                                        enum_symbols.append(value)
+                                        telemetry_definition['enum_symbols'] = enum_symbols
+                                        
         telemetry_list.append(telemetry_definition)
          
     # build a hashmap between group_channel IDs and name 
@@ -72,14 +92,29 @@ def parse_param(data: dict) -> list:
         group_list.append(group_dict)
     return group_list
     
-    
-def parse_dict(data: dict, top_key: str, main_key: str, optional_key: str = None) -> dict:
+
+def parse_dict(data: dict, top_key: str, main_key: str, optional_key: str = None, optional_key2: str = None) -> dict:
+    """ Parse specified keys in the given data dictionary
+        
+        Args:
+        data (dict): **[REQUIRED]** the json data to extract from
+        top_key (str): **[REQUIRED]** the top most key of the data dictionary
+        main_key (str): **[REQUIRED]** the primary key nested within the top most key
+        optional_key (str): any additional key nested within the top most key (default: none)
+        optional_key2 (str): any additional key nested within the top most key (default: none)
+
+    Returns:
+       A dictionary with the requested parsed items from the given input data
+    """
     parsed_dict = {}
     if top_key in data:
         for key, value in data[top_key][main_key].items():
             parsed_dict[key] = value
     if optional_key is not None:
         for key, value in data[top_key][optional_key].items():
+            parsed_dict[key] = value
+    if optional_key2 is not None:
+        for key, value in data[top_key][optional_key2].items():
             parsed_dict[key] = value
     return parsed_dict
 
