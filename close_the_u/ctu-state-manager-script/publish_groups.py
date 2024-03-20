ccf8ed7c94df5e04c8bd4be6e6d0ecf6daf59d97
Alex Alcazar <alcazar@mt-207601.jpl.nasa.gov>
2024-03-19 20:22:19 -0700

    States are first created then state ID is obtained to create groupMappings

--- close_the_u/ctu-state-manager-script/publish_groups.py
+++ close_the_u/ctu-state-manager-script/publish_groups.py
@@ -2,7 +2,7 @@ import xmltodict
 import argparse
 import json
 import close_the_u
-ENVIRONMENT = 'dev'
+ENVIRONMENT = 'local'
 
 
 def convert_xml(input_file: str) -> dict:
@@ -12,18 +12,18 @@ def convert_xml(input_file: str) -> dict:
         return data_dict
 
 
-def parse_channel(data: dict) -> list:
+def parse_channel(data: dict) -> tuple[list, list]:
     top_level_key = 'telemetry_dictionary'
-    telemetry_key = 'telemetry_definitions'
+    state_key = 'telemetry_definitions'
     enum_key = 'enum_definitions'
-    groups = parse_dict(data, top_level_key, telemetry_key, enum_key)
-    enum_list = []
-    
+    group_key= 'telemetry_groups'
+    parsed_data = parse_dict(data, top_level_key, state_key, enum_key, group_key)
+    state_list = []
     # create mapping of channel definitions and channel enums
-    for key in groups['telemetry']:
+    for key in parsed_data['telemetry']:
         channel_mapping = {
-            'channeID': None,
-            'groupName': None,
+            'channelId': None,
+            'identifier': None,
             'type': None,
             'ops_category': None,
             'description': None,
@@ -37,9 +37,9 @@ def parse_channel(data: dict) -> list:
         if isinstance(key, dict):
             for sub_key, value in key.items():
                 if sub_key == '@abbreviation':
-                    channel_mapping['channeID'] = value
+                    channel_mapping['channelId'] = value
                 elif sub_key == '@name':
-                    channel_mapping['groupName'] = value
+                    channel_mapping['identifier'] = value
                 elif sub_key == '@type':
                     channel_mapping['type'] = value
                 elif sub_key == 'categories':
@@ -54,7 +54,7 @@ def parse_channel(data: dict) -> list:
                 elif sub_key == 'enum_format':
                     enum_name = value['@enum_name']
                     # derive enums from enum name
-                    for key in groups['enum_table']:
+                    for key in parsed_data['enum_table']:
                         if isinstance(key, dict):
                             for sub_key, value in key.items():
                                 if sub_key == '@name' and value == enum_name:
@@ -66,33 +66,52 @@ def parse_channel(data: dict) -> list:
                                             enum_value = item['@numeric']
                                             channel_mapping['enumerations'][enum_member] = enum_value
 
-        enum_list.append(channel_mapping)
-    return enum_list
-    
-    
-def parse_param(data: dict) -> list:
-    top_level_key = 'param-def'
-    parameter_group = 'param'
-    enum_key = 'enum_definitions'
-    groups = parse_dict(data, top_level_key, parameter_group, enum_key)
-    enum_list = []
-    # derive definition first to obtain enum name
-    for item in groups['list']:
-        parameter_mapping = {
-            'channeID': None,
+        state_list.append(channel_mapping)
+        
+    # derive groups from channel_groups
+    group_list = []
+    for item in parsed_data['group']:
+        group = {
             'groupName': None,
+            'channelMappings': []
+        }
+        if isinstance(item, dict):
+            for key, value in item.items():
+                if key == '@group_name':
+                    group['groupName'] = value
+                if key == 'group_channel':
+                    for item in value:
+                        group['channelMappings'].append(item)
+                        
+        group_list.append(group)
+    return (state_list, group_list)
+    
+    
+def parse_param(data: dict) -> tuple[list, list]:
+    top_level_key = 'param-def'
+    state_key = 'param'
+    enum_key = 'enum_definitions'
+    group_key = 'parameter_groups'
+    parsed_data = parse_dict(data, top_level_key, state_key, enum_key, group_key)
+    state_list = []
+    # derive parameter definitions first to obtain enum name
+    for item in parsed_data['list']:
+        parameter_mapping = {
+            'channelId': None,
+            'identifier': None,
             'type': None,
             'ops_category': None,
             'description': None,
             'units': None,
             'enumerations': {}
         }
+         # derive parameter definitions first to obtain enum name
         if isinstance(item, dict):
             for key, value in item.items():
                 if key == '@param_id':
-                    parameter_mapping['channeID'] = value
+                    parameter_mapping['channelId'] = value
                 elif key == '@param_name':
-                    parameter_mapping['groupName'] = value
+                    parameter_mapping['identifier'] = value
                 elif key == '@type':
                     parameter_mapping['type'] = value
                 elif key == 'categories':
@@ -113,7 +132,7 @@ def parse_param(data: dict) -> list:
                     if isinstance(value, dict):
                         enum_name = value.get('enum_param', {}).get('@enum_name', '')
                         # derive enums from enum name
-                        for key in groups['enum_table']:
+                        for key in parsed_data['enum_table']:
                             if isinstance(key, dict):
                                 for sub_key, value in key.items():
                                     if sub_key == '@name' and value == enum_name:
@@ -124,12 +143,35 @@ def parse_param(data: dict) -> list:
                                                 enum_member = item['@symbol']
                                                 enum_value = item['@numeric']
                                                 parameter_mapping['enumerations'][enum_member] = enum_value
-                                        
-        enum_list.append(parameter_mapping)
-    return enum_list
+
+        state_list.append(parameter_mapping)
+        
+    # derive groups from parameter_groups
+    group_list = []
+    for item in parsed_data['parameter_group']:
+        group = {
+            'identifier': None,
+            'groupMappings': []
+        }
+        if isinstance(item, dict):
+            for key, value in item.items():
+                if key == '@param_group_name':
+                    group['identifier'] = value
+                if key == 'group_params':
+                    inner_values = value.get('group_param')
+                    for item in inner_values:
+                        group_map = {
+                            'identifier': item,
+                            'itemType': 'State'
+                        }
+                        group['groupMappings'].append(group_map)
+        
+        group_list.append(group)
+                        
+    return (state_list, group_list)
     
 
-def parse_dict(data: dict, top_key: str, main_key: str, optional_key: str = None) -> dict:
+def parse_dict(data: dict, top_key: str, main_key: str, optional_key: str = None, optional_key2: str = None) -> dict:
     """ Parse specified keys in the given data dictionary
         
         Args:
@@ -137,6 +179,7 @@ def parse_dict(data: dict, top_key: str, main_key: str, optional_key: str = None
         top_key (str): **[REQUIRED]** the top most key of the data dictionary
         main_key (str): **[REQUIRED]** the primary key nested within the top most key
         optional_key (str): any additional key nested within the top most key (default: none)
+        optional_key2 (str): any additional key nested within the top most key (default: none)
 
     Returns:
        A dictionary with the requested parsed items from the given input data
@@ -153,6 +196,9 @@ def parse_dict(data: dict, top_key: str, main_key: str, optional_key: str = None
     if optional_key is not None:
         for key, value in data[top_key][optional_key].items():
             parsed_dict[key] = value
+    if optional_key2 is not None:
+        for key, value in data[top_key][optional_key2].items():
+            parsed_dict[key] = value
     return parsed_dict
 
 
@@ -168,18 +214,18 @@ def read_from_json(filename: str) -> dict:
 
 
 def create_states(collection_id: str, state_records: dict, valueType: str) -> None:
-    new_states = state_records['data']
     states = []
-    for state in new_states:
+    for state in state_records:
         if isinstance(state, dict):
             record = {}
-            record['channelId'] = state['channeID']
+            record['channelId'] = state['channelId']
             record['restricted'] = False
-            record['identifier'] = state['groupName']
+            record['identifier'] = state['identifier']
             record['type'] = valueType
             record['dataType'] = state['type']
             record['subsystem'] = state['ops_category']
             record['source'] = 'flight'
+            record['description'] = state['description']
             states.append(record)
         if record['dataType'] == 'enum':
             # create state enumerations
@@ -189,32 +235,100 @@ def create_states(collection_id: str, state_records: dict, valueType: str) -> No
                 enum_name_value['label'] = key
                 enum_name_value['value'] = value
                 record['enumerations'].append(enum_name_value)
-        
+ 
     close_the_u.state_manager.create_states(collection_id, states, ENVIRONMENT)
-            
 
-def main(mode: str, input_file: str, output_file: str, collection_id: str) -> None:
+
+def map_channel_groups(channel_def: list, channel_groups: list) -> list:
+    channel_mapping = []
+    for item in channel_groups:
+        group = {
+            'identifier': None,
+            'groupMappings': []
+        }
+        if isinstance(item, dict):
+            group['identifier'] = item['groupName']
+            # for each group_channel find the State identifier in channel def
+            for group_channel in item['channelMappings']:
+                channel_id = group_channel
+                for definition in channel_def:
+                    group_mapping = {
+                    'itemIdentifier': None,
+                    'identifier': None,
+                    'itemType': 'State'
+                    }
+                    if definition['channelId'] == channel_id:
+                        # find the state identifier based on channelId
+                        group_mapping['identifier'] = definition['identifier']
+                        group['groupMappings'].append(group_mapping)
+                        break
+            channel_mapping.append(group)          
+    return channel_mapping                
+
+
+def find_state_identifier(state_identifier, state_dict) -> str:
+    identifier = ''
+    states = state_dict['data']['states']
+    for state in states:
+        for key, value in state.items():
+            if key == 'identifier' and value == state_identifier:
+                identifier = state['id']
+                print('state id found:', identifier)
+    return identifier
+             
+
+# Query the state IDs from SM and add it to the group mapping
+def generate_group_mapping(collection_id, group_dict) -> list:
+    states = {}
+    states = close_the_u.state_manager.get_states(collection_id, env=ENVIRONMENT)
+    groups = []
+    # Find each state id for each given group
+    for group in group_dict['group']:
+        this_group = {}
+        this_group['identifier'] = group['identifier']
+        this_group_mapping = []
+        for key, value in group.items():
+            if key == 'groupMappings':
+                for item in value:
+                    the_mapping = {}
+                    for inner_key, inner_value in item.items():
+                        if inner_key == 'identifier':
+                            state_identifier = inner_value
+                            id = find_state_identifier(state_identifier, states)
+                            the_mapping['itemIdentifier'] = id
+                            the_mapping['identifier'] = state_identifier
+                        the_mapping['itemType'] = 'State'
+                    this_group_mapping.append(the_mapping)
+        this_group['groupMappings'] = this_group_mapping
+        groups.append(this_group)
+    
+    return groups
+
+
+def main(mode: str, input_file: str, collection_id: str) -> None:
     # currently script will simply parse channel or param files
     data = convert_xml(input_file)
     valueType = ''
     if mode == 'channel':
-        output = parse_channel(data)
+        state_output, group_output = parse_channel(data)
         valueType = 'channel'
+        groups = map_channel_groups(state_output, group_output)
     if mode == 'parameter':
-        output = parse_param(data)
+        state_output, group_output = parse_param(data)
         valueType = 'fsw_parameter'
-        
-    group_dict = {'data': output}
-    write_to_output_file(group_dict, output_file)
-    json_data = read_from_json(output_file)
-    create_states(collection_id, json_data, valueType)
+        groups = group_output
+    group_dict = {'group': groups}
+    # Create the states in SM to generate an ID
+    create_states(collection_id, state_output, valueType)
+    # For each state in a group find its associated ID in SM 
+    groups = generate_group_mapping(collection_id, group_dict)
+    close_the_u.state_manager.create_groups(collection_id, groups, ENVIRONMENT)
         
         
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(description="close-the-u wrapper script")
     arg_parser.add_argument('-m', '--mode', dest='mode', choices=['channel', 'parameter'], required=True, help='run mode')
     arg_parser.add_argument('-i', '--input', dest='input_file', required=True, help='input file')
-    arg_parser.add_argument('-o', '--output', dest='output_file', default='output.json', required=False, help='output file')
     arg_parser.add_argument('-c', '--collectionId', dest='collection_id', required=True, help='collection id to be used')
     args = arg_parser.parse_args()
-    main(args.mode, args.input_file, args.output_file, args.collection_id)
+    main(args.mode, args.input_file, args.collection_id)
