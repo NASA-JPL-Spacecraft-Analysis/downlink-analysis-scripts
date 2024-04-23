a519e2bd21bb56a6c999724f40a8718d339faedb
Alex Alcazar <alcazar@mt-207601.jpl.nasa.gov>
2024-04-23 15:54:50 -0700

    Added fault monitors to SM constraints

--- close_the_u/ctu-state-manager-script/ctu_sm_script.py
+++ close_the_u/ctu-state-manager-script/ctu_sm_script.py
@@ -40,14 +40,17 @@ def main(mode: str, input_file: str, collection_id: str) -> None:
     if mode == 'flight_rules':
         constraints = parse_flight_rules(data, collection_id)
         create_constraints(collection_id, constraints)
+    if mode == 'fault_monitors':
+        constraints = parse_fault_monitors(data, collection_id)
+        create_constraints(collection_id, constraints)
     if mode == 'evr':
         events = parse_evr(data, collection_id)
         create_events(collection_id, events)
-        
+    
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser(description="close-the-u wrapper script")
     arg_parser.add_argument('-i', '--input', dest='input_file', required=True, help='input file: REQUIRED')
     arg_parser.add_argument('-c', '--collectionId', dest='collection_id', required=True, help='collection id to be used: REQUIRED')
-    arg_parser.add_argument('-m', '--mode', dest='mode', choices=['channel', 'parameter','flight_rules', 'evr'], required=True, help='Publish SM groups with channel and parameter. Publish SM constraints with flight_rules. Publish SM events with evr. mode:  REQUIRED')
+    arg_parser.add_argument('-m', '--mode', dest='mode', choices=['channel', 'parameter','flight_rules', 'evr', 'fault_monitors'], required=True, help='Publish SM groups with channel.xml or parameter.xml. Publish SM constraints with flight_rules.xml or fault_monitors.xml. Publish SM events with evr.xml. mode:  REQUIRED')
     args = arg_parser.parse_args()
     main(args.mode, args.input_file, args.collection_id)
--- close_the_u/ctu-state-manager-script/publish_constraints.py
+++ close_the_u/ctu-state-manager-script/publish_constraints.py
@@ -19,11 +19,35 @@ def parse_flight_rules(data: dict, collection_id: str) -> list:
             if key == '@level':
                 level = value
             if key == '@operational_category':
-                category = value
+                category = value.get('ops_category')
         rule_data['description'] = f'{level} {category} {description}'
         flight_rules.append(rule_data)
         
     return flight_rules
+
+
+def parse_fault_monitors(data: dict, collection_id: str) -> list:
+    fault_monitors = []
+    monitors = data['monitor_response_dictionary']['monitors']['monitor']
+    for monitor in monitors:
+        fault_data = {}
+        fault_data['collectionId'] = collection_id
+        fault_data['type'] = 'guideline'
+        fault_data['editable'] = True
+        for key, value in monitor.items():
+            if key == '@monitor_id':
+                fault_data['identifier'] = value
+            if key == '@monitor_name':
+                fault_data['displayName'] = value
+            if key == 'categories':
+                category = value.get('ops_category')
+            if key == '@monitor_description':
+                description = value
+        fault_data['description'] = f'{category} {description}'
+        fault_monitors.append(fault_data)
+        
+    return fault_monitors
+
 
 def create_constraints(collection_id: str, constraints: list):
     close_the_u.state_manager.create_constraints(collection_id, constraints, ENVIRONMENT)
