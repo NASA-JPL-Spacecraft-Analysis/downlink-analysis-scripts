9352815f154dce331a6fa0fabdf33fd00f5ccd77
Amy Pircher <amy.k.pircher@jpl.nasa.gov>
2024-05-14 18:21:23 -0700

    Fix eas-parasol dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     install_requires=[
         "m20-operational-cloud-store==7.7.1",
         "close-the-u @ git+https://github.jpl.nasa.gov/Europa-PESS/close-the-u-py.git",
-        "eas-parasol @ git+https://github.jpl.nasa.gov/397/parasol-py.git",
+        "eas-parasol>=8.0.0",
         "aerie_cli @ git+https://github.com/NASA-AMMOS/aerie-cli.git@main",
         "pandas",
         "dataclasses-json",
