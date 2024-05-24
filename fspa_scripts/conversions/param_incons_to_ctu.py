a0cf0af1fceaadba94c41fe57199f78b236246bf
Bennett Huffman <bennett.w.huffman@jpl.nasa.gov>
2024-05-24 10:20:27 -0700

    Add 'scet' time for CSDS input and remove 'param_json' from type choices for now

--- fspa_scripts/conversions/param_incons_to_ctu.py
+++ fspa_scripts/conversions/param_incons_to_ctu.py
@@ -55,7 +55,7 @@ def _insert_states(venue: str, states: List[Dict]) -> None:
             )
         )
 
-def build_param_json_states(collection_name, data) -> None:
+def build_param_json_states(collection_name, scet, data) -> None:
     """Build list of state data store objects from param_json parameters list."""
     # if incorrect JSON foramt, log and error
     if not 'parameter_file' in data or not 'parameter_list' in data['parameter_file']:
@@ -85,12 +85,12 @@ def build_param_json_states(collection_name, data) -> None:
 
         try:
             state = {
+                "scet": scet,
                 "collectionName": collection_name,
                 "hexId": parameter["id"],
                 "name": parameter["name"],
                 "version": parameter["version"],
                 "type": TYPE,
-                "scet": None, # TBD but looks like we need this field
                 "value": value if isinstance(value, numbers.Number) else -99999, # using same logic as publish_fsw_params.py
                 "value_type": VALUE_TYPE, # this should always be 'predicted' but it exists in param_json (parameter["value_type"].upper())
                 "metadata": metadata
@@ -105,7 +105,7 @@ def build_param_json_states(collection_name, data) -> None:
     return states
 
 
-def build_seqgen_fincon_states(collection_name, data) -> None:
+def build_seqgen_fincon_states(collection_name, scet, data) -> None:
     """Build list of state data store objects from seqgen_fincon JSON."""
     # if incorrect JSON foramt, log and error
     if not isinstance(data, list):
@@ -128,12 +128,12 @@ def build_seqgen_fincon_states(collection_name, data) -> None:
 
         try:
             state = {
+                "scet": scet,
                 "collectionName": collection_name,
                 "hexId": parameter["id"],
                 "name": parameter["name"],
                 "version": parameter["version"],
                 "type": TYPE,
-                "scet": None, # TBD but looks like we need this field
                 "value": parameter["value"] if isinstance(parameter["value"], numbers.Number) else -99999, # using same logic as publish_fsw_params.py
                 "value_type": VALUE_TYPE, # this should always be 'predicted'
                 "metadata": metadata
@@ -154,8 +154,9 @@ def main():
         description=PARSER_DESCRIPTION, 
         formatter_class=argparse.RawTextHelpFormatter
     )
-    parser.add_argument("-t", "--type", required=True, choices=['seqgen_fincon', 'param_json'], help="Type of input (seqgen_fincon or param_json) to publish.")
+    parser.add_argument("-t", "--type", required=True, choices=['seqgen_fincon'], help="Type of input (seqgen_fincon) to publish.") # removed 'param_json' type for now
     parser.add_argument("-i", "--input", required=True, help="Input file path for type.")
+    parser.add_argument("--scet", required=True, type=str, help="A SCET formatted time this input file was generated with that will be published to CSDS.")
     parser.add_argument("-c", "--collection", required=True, help="Name of the data store collection to publish parasol data (ex: eurcits001-578).")
     parser.add_argument("--env", default="dev", help="Venue for retrieving parameter values and publishing (ex: dev)")
     args = parser.parse_args()
@@ -168,9 +169,9 @@ def main():
 
     states = []
     if args.type == 'param_json':
-        states = build_param_json_states(args.collection, data)
+        states = build_param_json_states(args.collection, args.scet, data)
     elif args.type == 'seqgen_fincon':
-        states = build_seqgen_fincon_states(args.collection, data)
+        states = build_seqgen_fincon_states(args.collection, args.scet, data)
     else:
         logging.error("Unrecognized 'type'. Please see 'param_incons_to_ctu -h' for help.")
         sys.exit()
